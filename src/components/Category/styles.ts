c86f8493cc669150a252520cb7fcec01bd63c039
jhonatagarcia <jhonata93sg@gmail.com>
2021-07-08 19:40:29 -0300

    Alterações tela do perfil

--- src/components/Category/styles.ts
+++ src/components/Category/styles.ts
@@ -14,15 +14,14 @@ export const styles = StyleSheet.create({
     content: {
         width: 100,
         height: 116,
-        backgroundColor: theme.colors.secondary40,
         borderRadius: 8,
-        justifyContent: 'space-between',
         alignItems: 'center',
-        paddingVertical: 7
+        justifyContent: 'space-between',
+        paddingVertical: 20
     },
 
     title: {
-        fontFamily: theme.fonts.title500,
+        fontFamily: theme.fonts.title700,
         color: theme.colors.heading,
         fontSize: 15
     },
